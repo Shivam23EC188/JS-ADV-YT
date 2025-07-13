6d537d112834e0a66c57f93959bf0bdad91a089a
Shivam <shivamusha049@gmail.com>
2025-07-13 20:12:28 +0000

    variables

---
+++ 02_variables/01_variables.js
@@ -0,0 +1,20 @@
+"use strict"; // this use strict means treat all the js code as the newer version code of JS
+
+// alert(3+3); // we are using node js not browser
+
+// data types in js
+
+
+// 1) Primitive data-type
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// sybmol => used for defining uniqueness (Mostly used in react-js)
+
+
+// 2) Non-primitive data-type
+
+console.log(typeof(null)); // null is a object in js
+console.log(typeof(undefined)); // undefined bcs undefined is a type
