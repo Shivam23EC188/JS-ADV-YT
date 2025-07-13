1d489989a550ea754cf235d30c0f9a74d23d41a7
Shivam <shivamusha049@gmail.com>
2025-07-13 20:29:11 +0000

    Conversionsp-1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 2005
+let accountName = "Shivam";
+var accountPass = "@@@@@"
+accountCity = "Delhi"
+
+// console.log(accountID);
+// accountID = 2006
+console.log(accountID);
+
+console.table([accountID,accountName,accountPass,accountCity]);
+
+/*Prefer not to use var bcs of functional scope and block scope */
---
+++ 03_conversions/03_typeof.js
@@ -0,0 +1,16 @@
+// let score = 33;
+// let score = "33";
+// let score = "33";
+// let score = "33abc";
+
+// console.log(typeof(score));
+
+// let changetoNum = Number(score);
+
+// console.log(typeof(changetoNum));
+
+// 33=> 33 easily converted
+// if 33abc=> converted to Number then => NaN will be given
+// "" => empty string false
+
+
